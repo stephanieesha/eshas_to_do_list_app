512a0d70463d0003a5b47571bad904f74f764ef8
stephanieesha <52897969+stephanieesha@users.noreply.github.com>
2019-07-15 02:37:48 +0100

    Add files via upload

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header class="header"> 
+        <h1>ESHA'S TO DO LIST</h1>
+        <div id="nav">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+    </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+.header {
+    background: #333;
+    color: #fff;
+    text-align: centre;
+    padding: 10px;
+
+}
+.header a {
+    color: #fff;
+    padding-right: 5px;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About This App</h1>
+    <p>The Esha's To Do List app is an app that helps Esha keep track of things she needs to do.</p>
+  </div>
+</template>
